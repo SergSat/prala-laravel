1274780b991d3c89ce72bb3f02b18ed6a6a1b13f
Ihor <djigorsan@gmail.com>
2024-02-01 12:16:21 +0200

    fix bags on login gape

--- resources/views/pages/login-alt.blade.php
+++ resources/views/pages/login-alt.blade.php
@@ -1,75 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-		<title>Laravel</title>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-		<!-- Fonts -->
-		<link rel="preconnect" href="https://fonts.bunny.net" />
-		<link
-			href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap"
-			rel="stylesheet" />
+	<title>Laravel</title>
 
-		<!-- Stylesheet -->
-		@vite('resources/css/app.css')
-	</head>
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.bunny.net" />
+	<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-	<body class="font-light">
-		<div
-			class="h-dvh w-full bg-cover bg-center bg-no-repeat"
-			style="background-image: url({{ Vite::asset('resources/images/enter.png') }})">
-			<div class="mx-auto flex h-dvh w-4/5 flex-col sm:max-w-[648px]">
-				<img
-					width="350"
-					height="153"
-					src="{{ Vite::asset('resources/images/prala_logo1.svg') }}"
-					alt="logo-prala"
-					class="w-26 mx-auto mt-16 h-11 sm:h-20 sm:w-40 md:h-28 md:w-80" />
-				<hr class="mt-14 h-px border-0 bg-white/50" />
-				<form action="">
-					<div class="mt-2 text-center text-white">
-						<h1 class="text-3xl font-extralight tracking-widest">Вхід</h1>
-					</div>
+	<!-- Stylesheet -->
+	@vite('resources/css/app.css')
+</head>
 
-					<div
-						class="mt-10 w-full rounded-2xl border border-white/50 bg-white/15 px-6 pb-20 pt-9 backdrop-blur min-[300px]:px-10 min-[480px]:px-12 sm:px-16 sm:pb-20">
-						<div class="flex flex-col items-center justify-between gap-16">
-							<div
-								class="flex w-full flex-col items-start justify-between gap-5 md:justify-around lg:gap-10">
-								<label for="login" class="sr-only">Login</label>
-								<div class="w-full md:mt-2">
-									<input
-										type="text"
-										name="login"
-										id="login"
-										class="ext-lg w-full border border-b border-solid border-transparent border-b-white bg-transparent text-pr-blue placeholder-white placeholder:text-lg placeholder:font-extralight focus:border-b-2 focus:border-transparent focus:border-b-pr-blue focus:outline-none focus:ring-0"
-										placeholder="Логін" />
-								</div>
-								<label for="login" class="sr-only">Password</label>
-								<div class="w-full md:mt-2">
-									<input
-										type="password"
-										name="password"
-										id="password"
-										class="w-full border border-b border-solid border-transparent border-b-white bg-transparent text-3xl text-pr-blue placeholder-white placeholder:text-lg placeholder:font-extralight focus:border-b-2 focus:border-transparent focus:border-b-pr-blue focus:outline-none focus:ring-0"
-										placeholder="Пароль" />
-								</div>
+<body class="font-light">
+	<div class="h-dvh w-full bg-cover bg-center bg-no-repeat"
+		style="background-image: url({{ Vite::asset('resources/images/enter.png') }})">
+		<div class="mx-auto flex h-dvh w-4/5 flex-col sm:max-w-[648px]">
+			<img width="350" height="153" src="{{ Vite::asset('resources/images/prala_logo1.svg') }}" alt="logo-prala"
+				class="w-26 mx-auto mt-16 h-11 sm:h-20 sm:w-40 md:h-28 md:w-80" />
+			<hr class="max-[380px]:mt-6 mt-14 h-px border-0 bg-white" />
+			<form action="">
+				<div class="mt-2 text-center text-white">
+					<h1 class="text-3xl font-extralight tracking-widest">Вхід</h1>
+				</div>
+
+				<div
+					class="mt-10 w-full rounded-2xl border border-white bg-white/15 px-6 pb-20 pt-9 backdrop-blur max-[380px]:px-10 max-[380px]:pb-10 max-[380px]:pt-8 min-[480px]:px-12 sm:px-16 sm:pb-40">
+					<div class="flex flex-col items-center justify-between gap-16">
+						<div class="flex w-full flex-col items-start justify-between gap-5 md:justify-around lg:gap-10">
+							<label for="login" class="sr-only">Login</label>
+							<div class="w-full md:mt-2">
+								<input type="text" name="login" id="login"
+									class="ext-lg w-full border border-b border-solid border-transparent border-b-white bg-transparent text-pr-blue placeholder-white placeholder:text-lg placeholder:font-extralight focus:border-b-2 focus:border-transparent focus:border-b-pr-blue focus:outline-none focus:ring-0"
+									placeholder="Логін" />
 							</div>
-
-							<div
-								class="group grid w-28 shrink-0 place-items-center rounded-lg border border-white bg-transparent hover:border-pr-blue/40 hover:bg-pr-blue/5 md:w-36">
-								<a
-									href="http://dev.prala-laravel.com/index"
-									class="w-full justify-self-center p-2 text-center text-xl font-extralight tracking-wider text-white transition-colors duration-300 focus:border-b-2 focus:border-pr-blue/40 focus:outline-none focus:ring-0 group-hover:text-pr-blue/60 md:p-4 lg:text-3xl">
-									Увійти
-								</a>
+							<label for="login" class="sr-only">Password</label>
+							<div class="w-full md:mt-2">
+								<input type="password" name="password" id="password"
+									class="w-full border border-b border-solid border-transparent border-b-white bg-transparent text-3xl text-pr-blue placeholder-white placeholder:text-lg placeholder:font-extralight focus:border-b-2 focus:border-transparent focus:border-b-pr-blue focus:outline-none focus:ring-0"
+									placeholder="Пароль" />
 							</div>
 						</div>
+
+						<div
+							class="group grid w-28 shrink-0 place-items-center rounded-lg border border-white bg-transparent hover:border-pr-blue/40 hover:bg-pr-blue/5 md:w-36">
+							<a href="http://dev.prala-laravel.com/index"
+								class="w-full justify-self-center p-2 text-center text-xl font-extralight tracking-wider text-white transition-colors duration-300 focus:border-b-2 focus:border-pr-blue/40 focus:outline-none focus:ring-0 group-hover:text-pr-blue/60 md:p-4 lg:text-3xl">
+								Увійти
+							</a>
+						</div>
 					</div>
-				</form>
-			</div>
+				</div>
+			</form>
 		</div>
-	</body>
+	</div>
+</body>
+
 </html>
