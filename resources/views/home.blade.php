15148857bd40c24f7dd0d0169721f052d8976b13
Ihor <djigorsan@gmail.com>
2024-02-12 12:42:19 +0200

    made mobile_head component in parts and add svg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,7 @@
 <article class="grid w-full grid-flow-row grid-cols-1 gap-5 lg:grid-cols-2 lg:pt-24">
 	<h1 class="sr-only">Мої завдання, навчання та новини</h1>
 
-	<div class="mx-auto mt-10 w-full lg:hidden">
-		<div class="flex items-center justify-center gap-x-5 sm:gap-x-12">
-			<img src="{{ Vite::asset('resources/images/photo-model.png') }}" alt="avatar" />
-			<div class="flex flex-col items-center">
-				<h3 class="text-2xl">Анастасія</h3>
-				<p class="mt-1 font-extralight text-stone-500">Кулінарний геній</p>
-				<div class="mt-3 flex w-3/4 items-center justify-between">
-					<img class="h-5 w-5" src="{{ Vite::asset('resources/images/starfull.svg') }}" alt="star" />
-					<img class="h-5 w-5" src="{{ Vite::asset('resources/images/starempty.svg') }}" alt="star" />
-					<img class="h-5 w-5" src="{{ Vite::asset('resources/images/starempty.svg') }}" alt="star" />
-					<img class="h-5 w-5" src="{{ Vite::asset('resources/images/starempty.svg') }}" alt="star" />
-				</div>
-			</div>
-		</div>
-	</div>
+	@include('parts.mobile_head')
 
 	<!-- Tasks -->
 	<section class="mx-auto flex flex-col lg:mx-0">
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,18 +1,13 @@
 <header class="">
 	<section class="relative py-5 lg:hidden">
-		<img
-			class="mx-auto"
-			src="{{ Vite::asset('resources/images/logo-prala.png') }}"
-			alt="logo-prala" />
-		<button
-			id="hamburger-button"
+		<img class="mx-auto" src="{{ Vite::asset('resources/images/logo-prala.png') }}" alt="logo-prala" />
+		<button id="hamburger-button"
 			class="absolute right-5 top-1/2 h-8 w-14 -translate-y-1/2 transform cursor-pointer text-4xl text-pr-blue">
 			<span
 				class="absolute right-1 top-4 -mt-0.5 h-1 w-8 rounded bg-pr-blue drop-shadow transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-2.5 before:rounded before:bg-pr-blue before:drop-shadow before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-2.5 after:rounded after:bg-pr-blue after:drop-shadow after:transition-all after:duration-500 sm:right-4"></span>
 		</button>
 	</section>
-	<section
-		id="mobile-menu"
+	<section id="mobile-menu"
 		class="fixed left-0 right-0 z-30 min-h-screen -translate-x-full bg-white px-5 pt-9 transition-all duration-500 sm:px-10 lg:hidden">
 		<h3 class="text-center text-2xl font-extralight text-stone-500">Меню</h3>
 		<nav>
@@ -38,7 +33,7 @@
 		class="animate-open-menu ggg hidden rounded-md drop-shadow-lg lg:ml-5 lg:mt-5 lg:block lg:w-max lg:bg-white lg:px-5">
 		<nav>
 			<ul class="flex gap-5 py-4 text-base xl:gap-7">
-				<li><a href="#" class="--pr-active">Кабінет</a></li>
+				<li><a href="/" class="--pr-active">Кабінет</a></li>
 				<li><a href="#">Бібліотека</a></li>
 				<li><a href="#">Налаштування</a></li>
 				<li><a href="#">Голосування</a></li>
