2835349067bc851ac8cf201a097ce54ec707df65
Ihor <djigorsan@gmail.com>
2024-02-14 21:47:33 +0200

    some changes in layout

--- resources/views/pages/blog-page.blade.php
+++ resources/views/pages/blog-page.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app2')
 
 @section('content')
-<article class="grid w-full grid-flow-row grid-cols-1 gap-5 xl:grid-cols-4 lg:pt-24">
+<article class="grid w-full grid-flow-row grid-cols-1 gap-5 lg:grid-cols-2 lg:pt-24">
     <h1 class="sr-only">Сторінка блогу</h1>
 
     @include('parts.mobile_head')
