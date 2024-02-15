2a6331759bb6a5d6eb4ab18b493a781a3a7ce8b0
Ihor <djigorsan@gmail.com>
2024-02-15 16:02:35 +0200

    add blog-page and blog post part

--- resources/views/pages/blog-page.blade.php
+++ resources/views/pages/blog-page.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app2')
 
 @section('content')
-<article class="grid w-full grid-flow-row grid-cols-1 gap-5 lg:grid-cols-2 lg:pt-24">
+<article class="w-full">
     <h1 class="sr-only">Сторінка блогу</h1>
 
     @include('parts.mobile_head')
 
     <!-- Posts -->
-    @include('parts.blog-posts')
-    @include('parts.blog-posts')
-    @include('parts.blog-posts')
-    @include('parts.blog-posts')
+    <div class="grid w-full grid-flow-row grid-cols-1 gap-5 pt-20 lg:grid-cols-2 lg:pt-24">
+        @include('parts.blog-posts')
+        @include('parts.blog-posts')
+        @include('parts.blog-posts')
+        @include('parts.blog-posts')
+    </div>
     <!-- Posts -->
-
-
 </article>
 @endsection
