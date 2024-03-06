128864b040343ed72a5ec0f409c33248dbf2a9f6
Ihor <djigorsan@gmail.com>
2024-03-06 14:23:54 +0200

    changed css styles on button for registration in registration.blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
                     <div
                         class="grid bg-transparent border border-white rounded-lg group shrink-0 place-items-center hover:border-pr-blue/40 hover:bg-pr-blue/5">
                         <x-button
-                            class="w-full px-2 py-1 text-xl tracking-wider text-center text-white transition-colors duration-200 justify-self-center font-extralight focus:border-b-2 focus:border-pr-blue/40 focus:outline-none focus:ring-0 group-hover:text-pr-blue/60 md:p-4 lg:text-3xl">
+                            class="w-full px-2 py-1 text-xl tracking-wider text-center text-white transition-colors duration-200 justify-self-center font-extralight focus:border-b-2 focus:border-pr-blue/40 focus:outline-none focus:ring-0 group-hover:text-pr-blue/60 md:p-3 lg:text-2xl">
                             Зареєструватись
                         </x-button>
                     </div>
