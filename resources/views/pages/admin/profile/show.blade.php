daf45ffb3c8b65d85fed2b207da651948e97708a
serg <artdecomplus@gmail.com>
2024-03-09 12:35:31 +0200

    templates part profile from jetstream

--- resources/views/pages/admin/profile/show.blade.php
+++ resources/views/pages/admin/profile/show.blade.php
@@ -9,14 +9,14 @@
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
+                @livewire('parts.admin.profile.update-profile-information-form')
 
                 <x-admin.section-border />
             @endif
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
                 <div class="mt-10 sm:mt-0">
-                    @livewire('profile.update-password-form')
+                    @livewire('parts.admin.profile.update-password-form')
                 </div>
 
                 <x-admin.section-border />
@@ -24,21 +24,21 @@
 
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
                 <div class="mt-10 sm:mt-0">
-                    @livewire('profile.two-factor-authentication-form')
+                    @livewire('parts.admin.profile.two-factor-authentication-form')
                 </div>
 
                 <x-admin.section-border />
             @endif
 
             <div class="mt-10 sm:mt-0">
-                @livewire('profile.logout-other-browser-sessions-form')
+                @livewire('parts.admin.profile.logout-other-browser-sessions-form')
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
                 <x-admin.section-border />
 
                 <div class="mt-10 sm:mt-0">
-                    @livewire('profile.delete-user-form')
+                    @livewire('parts.admin.profile.delete-user-form')
                 </div>
             @endif
         </div>
