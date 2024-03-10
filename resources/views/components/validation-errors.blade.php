0cec3eccdab52627be0eabd9bc74e4bcea3f221f
Ihor <djigorsan@gmail.com>
2024-03-10 15:11:39 +0200

    fix bugs in translate with points

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,12 +1,13 @@
 @if ($errors->any())
-    <div {{ $attributes }}>
-        <div class="px-4 py-2 rounded-sm text-sm border bg-rose-100 dark:bg-rose-400/30 border-rose-200 dark:border-transparent text-rose-600 dark:text-rose-400">
-            <div class="font-medium">{{ __('Whoops! Something went wrong.') }}</div>
-            <ul class="mt-1 list-disc list-inside text-sm">
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>         
+<div {{ $attributes }}>
+    <div
+        class="px-4 py-2 text-sm border rounded-sm bg-rose-100 dark:bg-rose-400/30 border-rose-200 dark:border-transparent text-rose-600 dark:text-rose-400">
+        <div class="font-medium">{{ __('whoops').'!' }} {{ __('something_went_wrong').'.'}}</div>
+        <ul class="mt-1 text-sm list-disc list-inside">
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+</div>
 @endif
