0bc72e44319e0b1763ff40f4ace967931a0f2da1
Ihor <djigorsan@gmail.com>
2024-03-14 20:39:17 +0200

    fix styles

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -7,9 +7,10 @@
     </div>
     @endif
     <!-- Form -->
-    <form method="POST" action="{{ route('password.email') }}" class="w-11/12 mx-auto md:5/6 max-w-80">
+    <form method="POST" action="{{ route('password.email') }}" class="w-11/12 mx-auto md:5/6 max-w-[500px]">
         @csrf
-        <div class="flex flex-col gap-3">
+
+        <div class="flex flex-col items-center gap-3 mt-7">
             <x-admin.label for="email" class="text-white text-md">{{ __('auth.email_address') }} <span
                     class=" text-rose-500">*</span>
             </x-admin.label>
@@ -17,7 +18,7 @@
                 class="w-full text-lg placeholder-white border border-b border-transparent border-solid bg-white/10 border-b-white text-pr-blue placeholder:text-lg placeholder:font-extralight focus:border-b focus:border-transparent focus:border-b-pr-blue focus:outline-none focus:ring-0"
                 required autofocus />
         </div>
-        <div class="flex justify-end mt-6">
+        <div class="flex justify-center mt-6">
             <x-admin.button
                 class="px-2 py-1 text-xl tracking-wider text-center text-white transition-colors duration-200 border border-white rounded-lg hover:border-pr-blue/40 hover:bg-pr-blue/5 justify-self-center font-extralight focus:border focus:rounded-lg focus:border-pr-blue/40 focus:text-pr-blue tfocus:outline-none focus:ring-0 hover:text-pr-blue/60 md:p-3 lg:text-2xl">
                 {{ __('auth.send_reset_link') }}
